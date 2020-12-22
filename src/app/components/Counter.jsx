0500690d7617479144dd9512e21c5115d07d7a55
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2020-12-22 18:24:03 +0530

    starting event

--- src/app/components/Counter.jsx
+++ src/app/components/Counter.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // class component
+// class state - only available for class component
+
+// STATE: A Component data, which can be mutated/changed
+// props are read only, state can be used for get and set
 
 class Counter extends React.Component {
     // ES.next
@@ -12,6 +16,21 @@ class Counter extends React.Component {
 
     static defaultProps = {
         startValue: 0
+    }
+
+    constructor(props) {
+        super(props) // mandatory
+
+        // initialize state 
+        this.state = {
+            counter : props.startValue
+        }
+    }
+
+    // react shall pass event object as its first argument
+    increment(e) {
+        console.log('increment called', e);
+        console.log('this value is ', this)
     }
 
     // keyword
@@ -25,6 +44,10 @@ class Counter extends React.Component {
             <div>
                 <h2>Counter</h2>
                 <p>Start Value {startValue}</p>
+
+                <p>Counter value {this.state.counter}</p>
+                {/* react events take callback as reference */}
+                <button onClick={this.increment}>+1</button>
             </div>
         )
     }
